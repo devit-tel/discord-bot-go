99c33d18fbbf146f03b2cbc67cf378395dd5cfdd
NV4RE <tosbodesz@gmail.com>
2020-02-26 16:41:44 +0700

    Send message when user join

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -31,6 +32,8 @@ var (
 	serverAddress        string
 )
 
+var dg *discordgo.Session
+
 // EmailPayload Email payload
 type EmailPayload struct {
 	Sender   string    `json:"sender"`
@@ -45,24 +48,28 @@ type EmailBody struct {
 	DiscordName string `json:"discord_name"`
 	ProfileName string `json:"profile_name"`
 	Role        string `json:"role"`
-	IssuedAt    string `json:"issued_at"`
 }
 
 // VerifyBody Verify Body
 type VerifyBody struct {
+	UserID      string `json:"user_id"`
 	ProfileName string `json:"profile_name"`
-	Role        string `json:"role"`
+	RoleID      string `json:"role_id"`
+	MessageID   string `json:"message_id"`
 	IssuedAt    string `json:"issued_at"`
 }
 
 func main() {
-	setupEnv()
-	go setupDiscord(discordToken)
-	go setupRouter(serverAddress)
+	var err error
 
-	for {
-		// Do some thing
+	setupEnv()
+	dg, err = setupDiscord(discordToken)
+	if err != nil {
+		log.Panicln(err)
 	}
+	r := setupRouter()
+
+	r.Run(serverAddress)
 }
 
 func setupEnv() {
@@ -75,7 +82,7 @@ func setupEnv() {
 	discordToken = getEnv("DISCORD_TOKEN", "")
 	discordChannelID = getEnv("DISCORD_CHANNEL_ID", "")
 	discordServerID = getEnv("DISCORD_SERVER_ID", "")
-	key = []byte(getEnv("PASSPHRASE", "P4S$W0Rd"))
+	key = []byte(getEnv("PASSPHRASE", "P4S$W0Rd_Th41_5i2e_32_by7E_long!"))[:32]
 	serverAddress = getEnv("SERVER_ADDRESS", ":8080")
 }
 
@@ -87,7 +94,7 @@ func getEnv(key string, defaultValue string) string {
 	return value
 }
 
-func setupRouter(address string) *gin.Engine {
+func setupRouter() *gin.Engine {
 	gin.DisableConsoleColor()
 	r := gin.Default()
 
@@ -97,19 +104,64 @@ func setupRouter(address string) *gin.Engine {
 
 	r.GET("/verify/:secret", func(c *gin.Context) {
 		secret := c.Param("secret")
-		encrypted := aes256.Decrypt(key, secret)
-		var emailBody EmailBody
+		encrypted, err := aes256.Decrypt(key, secret)
+		if err != nil {
+			c.String(500, "Cannot decrypt")
+			c.Error(err)
+			c.Abort()
+			return
+		}
 
-		if err := json.Unmarshal([]byte(encrypted), &emailBody); err != nil {
+		var verifyBody VerifyBody
+
+		if err := json.Unmarshal([]byte(encrypted), &verifyBody); err != nil {
 			c.String(500, "Cannot convert JSON")
 			c.Error(err)
 			c.Abort()
 			return
 		}
 
-	})
+		issuedAt, err := time.Parse(time.RFC3339, verifyBody.IssuedAt)
+		if err != nil {
+			c.String(500, "Invalid time format")
+			c.Error(err)
+			c.Abort()
+			return
+		}
 
-	r.Run(address)
+		if issuedAt.Add(5 * time.Minute).Before(time.Now()) {
+			c.String(500, "Token expired")
+			c.Error(errors.New("Token expired"))
+			c.Abort()
+			return
+		}
+
+		err = dg.GuildMemberNickname(discordServerID, verifyBody.UserID, verifyBody.ProfileName)
+		if err != nil {
+			c.String(500, "Cannot modify user's role")
+			c.Error(err)
+			c.Abort()
+			return
+		}
+
+		err = dg.GuildMemberRoleAdd(discordServerID, verifyBody.UserID, verifyBody.RoleID)
+		if err != nil {
+			c.String(500, "Cannot modify user's role")
+			c.Error(err)
+			c.Abort()
+			return
+		}
+
+		err = dg.MessageReactionAdd(discordChannelID, verifyBody.MessageID, ":partying_face:")
+		if err != nil {
+			c.String(500, "Cannot React to message, but you good to go!")
+			c.Error(err)
+			c.Abort()
+			return
+		}
+
+		c.String(200, "Welcome, please check your discord!")
+	})
 
 	return r
 }
@@ -135,6 +187,7 @@ func setupDiscord(token string) (*discordgo.Session, error) {
 	}
 
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(userJoin)
 
 	if dg.Open(); err != nil {
 		return nil, err
@@ -145,12 +198,47 @@ func setupDiscord(token string) (*discordgo.Session, error) {
 	return dg, nil
 }
 
+func userJoin(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
+	_, err := s.ChannelMessageSendEmbed(discordChannelID, &discordgo.MessageEmbed{
+		Title:       fmt.Sprintf("Hello `%s`, welcome to true e-logistics comunity :heart:", m.User.Username),
+		Description: "Please introduce yourself by send a message to this channel",
+		Color:       3071986,
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:  "Format",
+				Value: "```Nickname, Role Name email@true-e-logistics.com```",
+			},
+			{
+				Name:  "Role ID",
+				Value: "```md\n1. Admin\n2. Designer\n3. Developer\n4. Manager\n5. Mobile\n6. Research\n7. Sale\n8. Support\n9. Tester```",
+			},
+			{
+				Name:  "Example",
+				Value: "```Tod, Research, tossaporn_tem@true-e-logistics.com``",
+			}},
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func findRoleIDByName(name string, roles []*discordgo.Role) (string, error) {
+	for _, r := range roles {
+		if strings.ToLower(r.Name) == strings.ToLower(name) {
+			return r.ID, nil
+		}
+	}
+
+	return "", errors.New("Role not found")
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
+	if m.Author.ID == s.State.User.ID || m.ChannelID != discordChannelID {
 		return
 	}
 
-	if m.ChannelID != discordChannelID {
+	if m.GuildID != discordServerID {
 		log.Println("Wirdo talking to me, help!!", m.Author.Username, m.Author.Email, m.Content)
 		return
 	}
@@ -165,25 +253,45 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	profileName := strings.TrimSpace(splitedContent[0])
+	roleName := strings.TrimSpace(splitedContent[1])
+	email := strings.TrimSpace(splitedContent[2])
+
+	guildRoles, err := s.GuildRoles(discordServerID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	roleID, err := findRoleIDByName(roleName, guildRoles)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	verifyBytes, err := json.Marshal(VerifyBody{
-		ProfileName: splitedContent[0],
-		Role:        splitedContent[1],
-		IssuedAt:    time.Now().Format("YY-MM-DD HH:mm:ss"),
+		ProfileName: fmt.Sprintf("%s (%s)", profileName, email),
+		RoleID:      roleID,
+		IssuedAt:    time.Now().Format(time.RFC3339),
+		MessageID:   m.ID,
 	})
 
-	secret := aes256.Encrypt(key, verifyBytes)
+	secret, err := aes256.Encrypt(key, verifyBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	emailPayload :=
 		EmailPayload{
-			Receiver: splitedContent[2],
+			Receiver: email,
 			Sender:   "do-not-reply@mail.service.drivs.io",
 			Subject:  "Verify email for discord channel",
 			Data: EmailBody{
 				DiscordName: m.Author.Username,
 				VerifyLink:  fmt.Sprintf("%s/verify/%s", verifyServiceBaseURL, secret),
-				ProfileName: splitedContent[0],
-				Role:        splitedContent[1],
-				IssuedAt:    time.Now().Format("YY-MM-DD HH:mm:ss"),
+				ProfileName: profileName,
+				Role:        roleName,
 			},
 		}
 
@@ -192,7 +300,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Verify link sent to your email (%s)", splitedContent[2]))
+	_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Verify link sent to your email (%s)", email))
 	if err != nil {
 		log.Println(err)
 	}
--- pkg/aes256/aes256.go
+++ pkg/aes256/aes256.go
@@ -10,10 +10,10 @@ import (
 )
 
 // Encrypt return base64 encrypted of given data
-func Encrypt(key []byte, plaintext []byte) string {
+func Encrypt(key []byte, plaintext []byte) (string, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	// The IV needs to be unique, but not secure. Therefore it's common to
@@ -21,13 +21,13 @@ func Encrypt(key []byte, plaintext []byte) string {
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
+		return "", err
 	}
 
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 
-	return base64.URLEncoding.EncodeToString(ciphertext)
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
 }
 
 // Decrypt from base64 to decrypted string
